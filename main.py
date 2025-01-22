b8fd1516c6ffa8f4c80ad8f15353d4091db207ca
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-22 19:09:07 -0300

    :sparkles: feat: Better way to declare
    Because the word 'optional' would seem to imply that the value is optional

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def read_root():
     return {"Hello" : "World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id:int, q:Optional[str]= None):
+async def read_item(item_id:int, q:str | None = None):
     return {
         "item_id" : item_id,
         "q" : q
