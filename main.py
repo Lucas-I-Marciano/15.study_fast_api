d1157398ac2230c5afd301bebdf08a9b874e6466
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-23 07:03:55 -0300

    :sparkles: feat: Handling with predefined values

--- main.py
+++ main.py
@@ -36,3 +36,20 @@ def read_user_me():
 @app.get("/users/{user_id}")
 def read_user(user_id :str):
     return {"user_id": user_id}
+
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name : ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
