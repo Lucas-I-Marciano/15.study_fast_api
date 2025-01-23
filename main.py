e75f84a9b413c18e599d04688d9a528892504fdc
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-23 06:59:00 -0300

    :bug: fix: Right way

--- main.py
+++ main.py
@@ -29,11 +29,10 @@ def update_item(item_id:int, item:Item):
         "item_id": item_id
     }
 
+@app.get("/users/me")
+def read_user_me():
+    return {"user_id": "the current user"}
 
 @app.get("/users/{user_id}")
 def read_user(user_id :str):
     return {"user_id": user_id}
-
-@app.get("/users/me")
-def read_user_me():
-    return {"user_id": "the current user"}
