7853ff53e6c2b0ab5589222ee2afc9cf111cf2eb
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-27 17:42:28 -0300

    :sparkles: feat: Excluding field fullfilled with default values
    If the data has the same values as the default ones, and they were set explicitly, they will be included in the JSON response

--- main.py
+++ main.py
@@ -321,6 +321,6 @@ class BaseUser(BaseModel):
 class UserIn2(BaseUser):
     password: str
 
-@app.post('/users-formated-response/')
+@app.post('/users-formated-response/', response_model_exclude_unset=True)
 async def create_user_formated(user:UserIn2) -> BaseUser:
     return user # It will return only username, email and full_name
