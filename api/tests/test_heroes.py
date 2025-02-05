ce7e3c8d76dc7828213a9db0294484da8ad0ec65
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-05 17:54:07 -0300

    :test_tube: test: Creating folder dedicated to test Heroes table and interaction

---
+++ api/tests/test_heroes.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from ..main import app
+
+client = TestClient(app)
+
+
+def test_reading_heroes():
+    response = client.get("/heroes")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
